2e081222f17180b5e262faa06b7d18f7de8ef83e
madamski9 <maciek.adamski9@gmail.com>
2025-01-14 21:46:33 +0100

    package locker added

--- my-app/pages/mainPage.js
+++ my-app/pages/mainPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, act } from "react";
 import { useRouter } from "next/router";
 import { useSearchParams } from "next/navigation";
 
@@ -192,7 +192,65 @@ const mainPage = () => {
                     </div>
                 )
             case "Package locker":
-                return <div>Package locker</div>
+                //! Paczkomat
+                const lockerLayout = [
+                    "top",
+                    "large l", "large", "large", "large", "large", "large", "large", "large r",
+                    "normal l", "normal","normal","normal", "double-small", "double-small", "normal", "normal r",
+                    "normal l", "normal","normal","normal", "double-small", "double-small", "normal", "normal r", 
+                    "normal l", "normal","normal","normal", "double-small", "double-small", "normal", "normal r", 
+                    "normal l", "normal","normal","normal", "double-small", "double-small", "normal", "normal r",
+                    "normal l", "normal", "normal", "normal","double-small","double-small", "normal", "normal r",
+                    "large ld", "large d", "large d", "large d", "large d", "large d", "large d", "large rd",
+                  ]
+                  console.log(fetchPackage)
+                  return (
+                    <div className="locker-main">
+                        <div className="locker-packages">
+                            {fetchPackage.length > 0 ? (
+                                fetchPackage.map((pkg, index) => (
+                                    <div key={index}>
+                                        <button className="packages-locker">
+                                            <img className="truck" src="/images/delivery.png"/>
+                                            <p>Number: {pkg.number}</p>
+                                            <p>Name: {pkg.name}</p>
+                                            <p>Status: {pkg.status}</p>
+                                        </button>
+                                    </div>
+                                ))
+                            ) : (
+                                <p>No packages avaible</p>
+                            )}
+                        </div>
+                        <div className="locker-grid">
+                        {lockerLayout.map((type, index) => (
+                            type === "double-small" ? (
+                            <div key={index} className="locker double-small">
+                                <div 
+                                    className="small"
+                                    onClick={() => alert(`Kliknięto szafkę numer ${index * 20 + 1}`)}
+                                ></div>
+                                <div 
+                                    className="small"
+                                    onClick={() => alert(`Kliknięto szafkę numer ${index * 20 + 2}`)}
+                                ></div>
+                            </div>
+                            ) : type === "top" ? (
+                                <div 
+                                    key={index} 
+                                    className="locker top"
+                                ></div>
+                            ) : (
+                            <div
+                                key={index}
+                                className={`locker ${type}`}
+                                onClick={() => alert(`Kliknięto szafkę numer ${index + 1}`)}
+                            ></div>
+                            )
+                        ))}
+                        </div>
+                    </div>
+                  )
         }
     }
     const renderContentLongDiv = () => {
@@ -248,6 +306,10 @@ const mainPage = () => {
                                             <img className="logoutImg" src="/images/logout.png"/>
                                         </button>
                                     </div>
+                                    <div 
+                                        className="exitBigDiv"
+                                        onClick={() => handleUserInfoClick()}
+                                    ></div>
                                 </>
                             )}
                         </div>
